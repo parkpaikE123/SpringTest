fb9062ee77958ddf2052693bfe787daf9a2704e5
ryujm <wjdals2285@naver.com>
2025-02-10 21:27:42 +0900

    [류정민] -Spring test 1 완성

--- src/main/java/com/ryujm/spring/test/test01.java
+++ src/main/java/com/ryujm/spring/test/test01.java
@@ -13,6 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class test01 {
 	
 	@RequestMapping("/1")
+	public String testProject() {
+		return "<h1>테스트 프로젝트 완성</h1>"
+				+ "<h2>해당 프로젝트를 통해서 문제 풀이를 진행 합니다.</h2>";
+	}
+	
+	
+	@RequestMapping("/2")
 	public Map<String, Integer> mapResponse() {
 		Map<String, Integer> gradeMap = new HashMap<>();
 		
