dd30ba79da67bc26a0b59c7488e3eee1f471f096
ryujm <wjdals2285@naver.com>
2025-02-18 10:28:13 +0900

    [류정민] -mvc test01 수정

--- src/main/java/com/ryujm/spring/test/mvc/SellerController.java
+++ src/main/java/com/ryujm/spring/test/mvc/SellerController.java
@@ -3,6 +3,7 @@ package com.ryujm.spring.test.mvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,13 +19,13 @@ public class SellerController {
 	private SellerService sellerService;
 	
 	// input 주소 연결
-	@GetMapping("/input")
+	@PostMapping("/input")
 	public String inputSeller() {
 			
 		return "/mvc/sellerInput";
 	}
 	// input 기능
-	@GetMapping("/create")
+	@PostMapping("/create")
 	public String inputSeller1(
 								@RequestParam("nickname") String nickname
 								, @RequestParam("temperature") double temperature
