4c51bf9b0f2e541e8a7c553e05cabab7632460a0
ryujm <wjdals2285@naver.com>
2025-02-21 21:36:50 +0900

    [류정민] -Thymeleaf 문자열 메소드를 활용하여 회원정보 리스트 정리

---
+++ src/main/java/com/ryujm/spring/test/thymeleaf/domain/WeatherHistory.java
@@ -0,0 +1,66 @@
+package com.ryujm.spring.test.thymeleaf.domain;
+
+import java.time.LocalDateTime;
+
+public class WeatherHistory {
+
+	private int id;
+	private String date;
+	private String weather;
+	private double temperatures;
+	private String microDust;
+	private double windSpeed;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getWeather() {
+		return weather;
+	}
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+	public double getTemperatures() {
+		return temperatures;
+	}
+	public void setTemperatures(double temperatures) {
+		this.temperatures = temperatures;
+	}
+	public String getMicroDust() {
+		return microDust;
+	}
+	public void setMicroDust(String microDust) {
+		this.microDust = microDust;
+	}
+	public double getWindSpeed() {
+		return windSpeed;
+	}
+	public void setWindSpeed(double windSpeed) {
+		this.windSpeed = windSpeed;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
+}
